b6973b1b3ca96cfe684d20c54fb2c3141f2f3b78
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-23 11:56:38 +0300

    Update MainActivity.java

--- src/main/java/com/stanrobert/manageractivitate/MainActivity.java
+++ src/main/java/com/stanrobert/manageractivitate/MainActivity.java
@@ -31,35 +31,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-/*
-===========================================================================================================================
-Cum se foloseste aplicatia:
-
-Pentru adaugare:
-data se pune in textbox                         01-01-0001
-ora se pune in textbox                          7-15 sau 6:00-9:30 sau 16:00-24
-pauza se pune in textbox                        fie 1 (daca e intreg) sau 0:30 daca sunt doar 30 min , 0:45 etc
-leii pe ora se pun in textbox                   12 sau 12.5
-
-Pentru adaugare:
-Toate textboxurile trebuiesc umplute
-
-Pentru modificare:
-Dai click pe rand (uri) si apoi umpli doar textboxurile care doresti sa modifici datele (ore la munca , ore pauza , lei/h)
-(data calendaristica va fi ignorata si va fi pastrata originalul) iar apoi apesi butonul modifica (care era butonul adaugare inainte)
-
-Pentru stergere:
-Dai click pe rand (uri) si apoi apesi butonul stergere
-
-Pentru backup/Vizionare pe lunile vechi
-in textboxul de data se pune data fara ziua (doar luna si an) ex: 01-2020   sau    03-2021
-
-Se salveaza fisiere in:
-Vizibil : Folder ManagerActivitate: 01.01.0001.txt
-Invizibil (Inaccesibil decat root sau pe PC) : data/user/0/com.stanrobert.manageractivitate   1 fisier:  backup data.txt
- */
-
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int REQUEST_WRITE_STORAGE = 112;
